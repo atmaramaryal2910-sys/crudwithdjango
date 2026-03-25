fb2e44a3e0062e197b73493f1337504fac6bb047
Atmaram Aryal <atmaramaryal2910@gmail.com>
2026-03-25 22:19:30 +0545

    Allow serving media files in production

--- football_project/urls.py
+++ football_project/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.views.static import serve
+import re
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('players.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in both development AND production (for this Render demo)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
