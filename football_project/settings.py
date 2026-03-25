60b9b84d7e46a50aab4a4da7a62549c3f4969588
Atmaram Aryal <atmaramaryal2910@gmail.com>
2026-03-25 22:11:02 +0545

    Fix Pillow build and ALLOWED_HOSTS

--- football_project/settings.py
+++ football_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-vz8@4x!fbkqs$d#pe)sff
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
